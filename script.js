dc6a1c0ff48a190c76688805fe17a1bf047751f8
Andrew Denson <denson10@gmail.com>
2021-05-11 18:56:26 -0400

    adding new search function for the student page

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-﻿
+﻿$(document).ready(function(){
+
+  let api_key = "AIzaSyBjeB7NxJ34NhgJIi5swpWT7rEgudCUCO0";
+  let video = '';
+  var videos = $("#videos");
+
+  $("#form").submit(function(e){
+      e.preventDefault()
+      let search = $("#search").val();
+
+      videoSearch(api_key,search,3);
+  })
+
+  function videoSearch(api_key, search, maxResults){
+      videos.empty();
+      $.get("https://www.googleapis.com/youtube/v3/search?key=" + api_key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,function(data){
+          console.log(data);
+
+          data.items.forEach(item => {
+              video = `
+              <iframe width="315" height="200" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>`
+              
+              videos.append(video);
+          })
+
+      })
+
+  }
+
+});
+
 // * This code is for the valadation for are login part
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
@@ -20,3 +50,7 @@
         }, false)
       })
   })()
+
+
+
+  
